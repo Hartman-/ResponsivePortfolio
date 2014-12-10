95c83e7ad97042db226ce5ad0172ce1bdb279336
Hartman- <thehartman8@gmail.com>
2014-12-10 01:30:22 -0500

    Updated mobile styling + added contact form

--- js/portfolio-scripts.js
+++ js/portfolio-scripts.js
@@ -1,42 +1,77 @@
 $(document).ready(function() {
-	
-  var pageSwiper = $('#landing-section .swiper-container').swiper({
-    //Your options here:
-    mode:'horizontal',
-    loop: true,
-    calculateHeight: true
-    //etc..
-  });
+
+    // ===
+    // INITIALIZATION
+    // ===
+
+    // Create slider on load (first thing in order for calculations to work)	
+    var pageSwiper = $('#landing-section .swiper-container').swiper({
+        //Your options here:
+        mode:'horizontal',
+        speed: 1000,
+        loop: true,
+        calculateHeight: true,
+        simulateTouch: false,
+        onSlideChangeStart: function() {
+            $('nav.page-nav a').removeClass('active');
+            $('nav.page-nav a').eq(pageSwiper.activeIndex - 1).addClass('active');
+        }
+    });
+
+    // Center content on load
+    var vertPadding = calcPadding();
+    $('.container #landing-section .jumbotron').css('top', vertPadding+"px");
 
 
-	var vertPadding = calcPadding();
-	$('.container #landing-section .jumbotron').css('top', vertPadding+"px");
+
+    // ===
+    // RESIZE WINDOW - CENTER CONTENT
+    // ===
 
 	$(window).resize(function() {
 		var vertPadding = calcPadding();
 		$('.container #landing-section .jumbotron').css('top', vertPadding+"px");
 	});
 
-  function calcPadding() {
-    amtPadding = ($('.container #landing-section').height() - $('.container #landing-section .jumbotron').height() - 50) / 2;
-    return amtPadding;
-  }
+    function calcPadding() {
+        amtPadding = ($('.container #landing-section').height() - $('.container #landing-section .jumbotron').height() - 50) / 2;
+        return amtPadding;
+    }
+
+    // ===
+    // END WINDOW CENTERING
+    // ===
+
+
 
 	// ===
 	// SCROLLING FUNCTIONALITY
 	// ===
 
-	function swipeTarget(target) {
+    function swipeTarget(target) {
     	var duration = 1200;
-      pageSwiper.swipeTo(target, 1000);
+        pageSwiper.swipeTo(target, 1000);
   	}
 
-  	$('nav a').click(function(e) {
+  	$('nav.page-nav a').click(function(e) {
     	var target = $(this).index();
 
     	swipeTarget(target);
     	e.preventDefault();
   	});
+
+    $('span.nav-arrows').click(function(e) {
+        var target = $(this).data('dir');
+      
+        if(target == "left") {
+            pageSwiper.swipePrev();
+        }
+        else {
+            pageSwiper.swipeNext();
+        }
+
+        e.preventDefault();
+    });
 
   	// ===
   	// END SCROLLING
