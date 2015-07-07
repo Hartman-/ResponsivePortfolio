5b35045018c636a7f0ad0c2d4e92ffc4792594fa
Hartman- <thehartman8@gmail.com>
2015-07-07 01:20:19 -0400

    updated nav bar + added swiper

--- js/portfolio-scripts.js
+++ js/portfolio-scripts.js
@@ -53,107 +53,29 @@ $(document).ready(function() {
     // ===
 
     // Create slider on load (first thing in order for calculations to work)	
-    var pageSwiper = $('#landing-section .swiper-container.main-swiper').swiper({
+    var pageSwiper = new Swiper('.section .section-reel .swiper-container.reel-swiper', {
         //Your options here:
         mode:'horizontal',
         speed: 1000,
-        loop: false,
-        calculateHeight: true,
-        simulateTouch: false, 
-        onSlideChangeStart: function() {
-            $('nav.page-nav a').removeClass('active');
-            $('nav.page-nav a').eq(pageSwiper.activeIndex).addClass('active');
-
-            if(pageSwiper.activeIndex == 0) {
-
-                $('span.nav-arrows.left').fadeOut();
-                ga('send', 'event', 'Home', 'click', 'nav-buttons');
-            }
-            else if(pageSwiper.activeIndex == 1) {
-
-                $('span.nav-arrows.left').fadeIn(); 
-                ga('send', 'event', 'Work', 'click', 'nav-buttons');
-            }
-            else if(pageSwiper.activeIndex == 2) {
-
-                $('span.nav-arrows.left').fadeIn(); 
-                ga('send', 'event', 'About', 'click', 'nav-buttons');
-            }
-            else if(pageSwiper.activeIndex == 3) {
-
-                $('span.nav-arrows.left').fadeIn(); 
-                ga('send', 'event', 'Contact', 'click', 'nav-buttons');
-            }
-            else {
-                $('span.nav-arrows.left').fadeIn();   
-            }
-        },
-        preventLinksPropagation: true
-    });
-
-    var workSwiper = $('#landing-section .swiper-container.work-swiper').swiper({
-        //Your options here:
-        mode:'vertical',
-        speed: 1000,
         loop: true,
-        pagination: '.pagination',
+        pagination: '.swiper-pagination',
         paginationClickable: true,
-        calculateHeight: true,
-        cssWidthAndHeight: true,
+        simulateTouch: true,
         preventLinksPropagation: true
     });
-
-    // Center content on load
-    var vertPadding = calcPadding();
-    $('.container #landing-section .jumbotron').css('top', vertPadding+"px");
-    $('span.nav-arrows.left').hide();
-
-
-
-    // ===
-    // RESIZE WINDOW - CENTER CONTENT
-    // ===
-
-	$(window).resize(function() {
-		var vertPadding = calcPadding();
-		$('.container #landing-section .jumbotron').css('top', vertPadding+"px");
-	});
-
-    function calcPadding() {
-        amtPadding = ($('.container #landing-section').height() - $('.container #landing-section .jumbotron').height() - 50) / 2;
-        return amtPadding;
-    }
-
-    // ===
-    // END WINDOW CENTERING
-    // ===
-
-
 
 	// ===
 	// SCROLLING FUNCTIONALITY
 	// ===
 
-    function swipeTarget(target) {
-    	var duration = 1200;
-        pageSwiper.swipeTo(target, 1000);
-  	}
-
-  	$('nav.page-nav a').click(function(e) {
-    	var target = $(this).index();
-
-    	swipeTarget(target);
-    	e.preventDefault();
-  	});
-
     $('span.nav-arrows').click(function(e) {
         var target = $(this).data('dir');
       
         if(target == "left") {
-            pageSwiper.swipePrev();
+            pageSwiper.slidePrev();
         }
         else {
-            pageSwiper.swipeNext();
+            pageSwiper.slideNext();
         }
 
         e.preventDefault();
@@ -178,7 +100,7 @@ $(window).scroll(function() {
     }
     var current = '#home';
     if($(window).scrollTop() >= $('a[name=about]').parent().offset().top-$('.nav').height()) current = '#about';
-    if($(window).scrollTop() >= $('a[name=reel]').parent().offset().top-$('.nav').height()) current = '#reel';
+    if($(window).scrollTop() >= $('a[name=reel]').parent().offset().top-$('.nav').height())  current = '#reel';
     if($(window).scrollTop() >= $('a[name=contact]').parent().offset().top-$('.nav').height()) current = '#contact';
     if($(window).scrollTop() >= $(document).height()-$('.landing').height()) {
         current = '#contact';
